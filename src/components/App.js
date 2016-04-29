6f830bdeeb6052819f0a1aaf7092cd673f5fb819
albert.oclarit <bbjao143213620>
2016-04-29 17:25:52 +0800

    Initial Login and Register

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,63 @@
 import React from 'react';
+import {
+       Well,
+       FormGroup,
+       ControlLabel,
+       FormControl,
+       HelpBlock,
+       Button,
+       ButtonGroup
+  } from 'react-bootstrap'
 
-export default  class TodoApp extends React.Component{
+export default  class App extends React.Component{
  constructor(props){
      super(props);
  }
  
+ goToRegister(){
+  this.props.history.push("/register");    
+ }
+ 
  render(){
+      const wellStyle={
+        width:400,
+        height:500,
+        marginLeft:'auto',
+        marginRight:'auto'  
+      };
      return (
-         <div>
-         <h1>Welcome to ReactJS</h1>
+         <div className="container">
+         <Well style={wellStyle}>
+         <legend>Please Login</legend>
+         <form>
+          <FormGroup>
+            <ControlLabel>Enter Username</ControlLabel>
+            <FormControl
+                type="text"
+                placeholder="Enter your username"
+            />
+            <FormControl.Feedback />
+            <HelpBlock></HelpBlock>
+         </FormGroup>
+         <FormGroup>
+            <ControlLabel>Enter Password</ControlLabel>
+            <FormControl
+                type="password"
+                placeholder="Enter your password"
+            />
+            <FormControl.Feedback />
+            <HelpBlock></HelpBlock>
+         </FormGroup>
+         <div className="btncontainer">
+         <ButtonGroup>
+            <Button bsStyle="primary" type="submit">Login</Button>
+            <Button bsStyle="info" type="button"
+                    onClick={this.goToRegister.bind(this)}
+                    >Register</Button>
+         </ButtonGroup>
+         </div>
+         </form>
+         </Well>
          </div>
      );
  }
---
+++ src/components/Register.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Well} from 'react-bootstrap'
+
+export default  class Register extends React.Component{
+
+ render(){
+      const wellStyle={
+        width:400,
+        height:500,
+        marginLeft:'auto',
+        marginRight:'auto'  
+      };
+    return (
+          <div className="container">
+            <Well style={wellStyle}>
+            <legend>Please Register</legend>
+            </Well>
+         </div>
+    );
+ }
+ }
